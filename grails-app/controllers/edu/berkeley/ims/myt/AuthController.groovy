1902acb7b2ca429625a102a5a108cb2bb076d99f
Lucas Rockwell <lr@lucasrockwell.com>
2012-07-23 13:05:33 -0700

    Made changes for issue MMT-19, allow developer to bypass AuthN.

--- grails-app/controllers/edu/berkeley/ims/myt/AuthController.groovy
+++ grails-app/controllers/edu/berkeley/ims/myt/AuthController.groovy
@@ -14,7 +14,6 @@ class AuthController {
      * Displays the base login page for the app.
      */
     def index() {
-        //redirect(action:'login')
         if (session.person) {
             redirect(controller:'main', action:'index')
         }
@@ -27,43 +26,60 @@ class AuthController {
         if (session.person) {
             redirect(controller:'main', action:'index')
         }
+        else if (grails.util.Environment.current.name == "development" &&
+            grailsApplication.config.myt.autoLoginUserAs) {
+            def person = ldapService.find(
+                grailsApplication.config.ldap.personUsernameAttr,
+                grailsApplication.config.myt.autoLoginUserAs)
+            setSessionAndRedirect(person)
+        }
         else {
             def user = request.getHeader('REMOTE_USER') ?: request.getRemoteUser()
             if (!user) {
-                log.error("Did not get user from REMOTE_USER header...")
+                log.error "Did not get user from REMOTE_USER header..."
                 redirect(action:'failure')
             }
             else {
                 def person = ldapService.find(
                     grailsApplication.config.ldap.personUsernameAttr, user)
-                if (person) {
-                    session.person = person
-                    // Get these from the session. They were set by the
-                    // isAuthenticated SecurityFilter.
-                    if (session.intendedController) {
-                        redirect(
-                            controller:session.intendedController,
-                            action:session.intendedAction,
-                            params: session.intendedParams)
-                    }
-                    else {
-                        redirect(controller:'main', action:'index')
-                    }
-                }
-                else {
-                    redirect(action:'notAuthorized')
-                }
+                setSessionAndRedirect(person)
             }
         }
     }
 
+    private setSessionAndRedirect(person) {
+        if (person) {
+            session.person = person
+            // Get these from the session. They were set by the
+            // isAuthenticated SecurityFilter.
+            if (session.intendedController) {
+                redirect(
+                    controller:session.intendedController,
+                    action:session.intendedAction,
+                    params: session.intendedParams)
+            }
+            else {
+                redirect(controller:'main', action:'index')
+            }
+        }
+        else {
+            redirect(action:'notAuthorized')
+        }
+    }
+
     /**
-     * Invalidate the session, and send the user to the config.myt.logoutURL.
+     * Invalidate the session, and send the user to the config.myt.logoutURL, if one
+     * is specified.
      */
     def logout() {
         session.invalidate()
-        redirect(url:grailsApplication.config.myt.logoutURL)
-        return
+        if (grailsApplication.config.myt?.logoutURL) {
+            redirect(url:grailsApplication.config.myt.logoutURL)
+        }
+        else {
+            flash.logout = message(code:'auth.logout')
+            redirect(action:'index')
+        }
     }
 
     def notAuthorized() { }
