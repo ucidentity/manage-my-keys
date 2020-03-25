c9fe4ec3f80a2b3dc25c3c98afa3d1475b9dc6af
Jeff McCullough <jeffmc@berkeley.edu>
2020-03-25 13:06:07 -0600

    Upgrade unboundid library to .6

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -35,7 +35,7 @@ grails.project.dependency.resolution = {
 
         runtime 'mysql:mysql-connector-java:5.1.16'
         runtime 'postgresql:postgresql:9.1-901.jdbc4'
-        runtime 'com.unboundid:unboundid-ldapsdk:2.3.1'
+        runtime 'com.unboundid:unboundid-ldapsdk:2.3.6'
         //runtime 'org.jasig.cas:cas-client:3.1.10'
         compile 'com.google.apis:google-api-services-admin-directory:directory_v1-rev71-1.22.0'
 
